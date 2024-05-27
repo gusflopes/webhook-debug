539b6769a7dc4be52f04ee3eab686e880e75b0a8
Gustavo Lopes <gusflopes86@gmail.com>
2024-05-27 16:44:25 -0400

    update to proxy

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 const port = 6000;
+const targetUrl = 'https://126e-179-177-14-153.ngrok-free.app/api/bringoz/EntityServiceCreatedWebHook';
 
 // Middleware para parsear o body como JSON
 app.use(express.json());
 
 app.post('/webhook', (req, res) => {
-    console.log('Webhook received');
-    console.log("Headers:");
-    console.log(req.headers);
-    console.log("Body:");
-    console.log(req.body);
-    console.log('--------- FIM ---------');
-    res.status(200).send('Webhook received');
+  try {
+    const method = req.method;
+	const headers = {...req.headers };
+	delete headers.host;
+
+	const response = await axios({
+		method: method,
+		url: `${targetUrl}`,
+		headers: headers,
+		data: req.body
+	});
+
+	res.status(response.status).set(response.headers).send(response.data);
+  } catch (error) {
+		console.error(error);
+		res.status(500).send('Aconteceu um erro');
+	}
 });
 
 app.listen(port, () => {
